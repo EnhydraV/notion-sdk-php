0319cb336a31f9438129d7348958ee20ab63bd46
Vincent Vanwaelscappel <vincent@cubedesigners.com>
2023-01-10 18:56:57 +0100

    Allow null url

--- src/Common/File.php
+++ src/Common/File.php
@@ -13,82 +13,79 @@ use DateTimeImmutable;
  *
  * @psalm-immutable
  */
-class File
-{
-    private function __construct(
-        public readonly FileType $type,
-        public readonly string $url,
-        public readonly DateTimeImmutable|null $expiryTime,
-    ) {
-    }
+class File {
+	private function __construct(
+		public readonly FileType $type,
+		public readonly string $url,
+		public readonly DateTimeImmutable|null $expiryTime,
+		public readonly string $name,
+	) {
+	}
 
-    public static function createExternal(string $url): self
-    {
-        return new self(FileType::External, $url, null);
-    }
+	public static function createExternal(string $url): self {
+		return new self(FileType::External, $url, null, '');
+	}
 
-    public static function createInternal(
-        string $url,
-        DateTimeImmutable|null $expiryTime = null
-    ): self {
-        return new self(FileType::Internal, $url, $expiryTime);
-    }
+	public static function createInternal(
+		string $url,
+		DateTimeImmutable|null $expiryTime = null
+	): self {
+		return new self(FileType::Internal, $url, $expiryTime, '');
+	}
 
-    /**
-     * @param FileJson $array
-     *
-     * @internal
-     */
-    public static function fromArray(array $array): self
-    {
-        $type = $array["type"];
+	/**
+	 * @param FileJson $array
+	 *
+	 * @internal
+	 */
+	public static function fromArray(array $array): self {
+		$type = $array["type"];
 
-        $file = $array[$type] ?? [];
+		$file = $array[$type] ?? [];
 
-        return new self(
-            FileType::from($type),
-            $file["url"] ?? "",
-            isset($file["expiry_time"]) ? new DateTimeImmutable($file["expiry_time"]) : null,
-        );
-    }
+		return new self(
+			FileType::from($type),
+			$file["url"] ?? "",
+			isset($file["expiry_time"]) ? new DateTimeImmutable($file["expiry_time"]) : null,
+			$array['name'] ?? '',
+		);
+	}
 
-    public function toArray(): array
-    {
-        $array = [];
-        $type = $this->type;
+	public function toArray(): array {
+		$array = [];
+		$type = $this->type;
 
-        if ($type === FileType::Internal) {
-            $array = [
-                "type" => "file",
-                "file" => [
-                    "url" => $this->url,
-                    "expiry_time" => $this->expiryTime?->format(Date::FORMAT),
-                ],
-            ];
-        }
+		if ($type === FileType::Internal) {
+			$array = [
+				"name" => $this->name,
+				"type" => "file",
+				"file" => [
+					"url" => $this->url,
+					"expiry_time" => $this->expiryTime?->format(Date::FORMAT),
+				],
+			];
+		}
 
-        if ($type === FileType::External) {
-            $array = [
-                "type" => "external",
-                "external" => [ "url" => $this->url ],
-            ];
-        }
+		if ($type === FileType::External) {
+			$array = [
+				"name" => $this->name,
+				"type" => "external",
+				"external" => ["url" => $this->url],
+			];
+		}
 
-        return $array;
-    }
+		return $array;
+	}
 
-    public function isExternal(): bool
-    {
-        return $this->type === FileType::External;
-    }
+	public function isExternal(): bool {
+		return $this->type === FileType::External;
+	}
 
-    public function isInternal(): bool
-    {
-        return $this->type === FileType::Internal;
-    }
+	public function isInternal(): bool {
+		return $this->type === FileType::Internal;
+	}
 
-    public function changeUrl(string $url): self
-    {
-        return new self($this->type, $url, $this->expiryTime);
-    }
+	public function changeUrl(string $url): self {
+		return new self($this->type, $url, $this->expiryTime);
+	}
 }
